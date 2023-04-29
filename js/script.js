3338bb76cba8e4502dec97bdbdbf868717b0515e
jatvik0609 <jatvik0609@outlook.com>
2023-04-29 11:10:47 +0530

    navlist

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", this.window.scrollY > 100);
 });
+
+var navlist = document.getElementById("navlist");
+
+navlist.style.maxHeight = "0px";
+
+function togglemenu(){
+    if(navlist.style.maxHeight == "0px")
+    {
+        navlist.style.maxHeight = "130px";
+    }
+    else{
+        navlist.style.maxHeight = "0px";
+    }
+}
